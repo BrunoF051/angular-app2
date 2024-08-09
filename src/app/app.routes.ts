72b3f7454ef1c4d80c949a4472e43f742bcc732d
Bruno Frontini <b.frontiniganem@bssweb.it>
2024-08-09 15:04:05 +0200

    add routes comment

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -4,6 +4,10 @@ export const routes: Routes = [
   { path: '', pathMatch: 'full', redirectTo: '/app' },
   {
     path: 'app',
+    /*
+    This set the routes for Lazy loading
+    you can find the routes in /pages/welcome/welcome.routes
+    */
     loadChildren: () =>
       import('./pages/welcome/welcome.routes').then((m) => m.APP_ROUTES),
   },
